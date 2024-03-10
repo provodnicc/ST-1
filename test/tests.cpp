e8b2bb0f293851857bc0d48e961936af95e54cfa
Pavel Smirnov <pasha1201s@inbox.ru>
2024-03-10 12:34:57 +0300

    feat(lab1): init tests and number Lib

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,28 +5,22 @@
 
 
 TEST(PrimeTest, CheckPrimeWithPrimeNumber) {
-  uint64_t number = 13;
-  EXPECT_TRUE(checkPrime(number));
+  EXPECT_TRUE(checkPrime(11));
 }
 
 
 TEST(PrimeTest, CheckPrimeWithCompositeNumber) {
-  uint64_t number = 20;
-  EXPECT_FALSE(checkPrime(number));
+  EXPECT_FALSE(checkPrime(20));
 }
 
 
 TEST(PrimeTest, NthPrimeNumber) {
-  uint64_t number = 6;
-  uint64_t expected = 13;
-  EXPECT_EQ(expected, nPrime(n));
+  EXPECT_EQ(13, nPrime(5));
 }
 
 
 TEST(PrimeTest, NextPrimeNumber) {
-  uint64_t value = 24;
-  uint64_t expected = 29;
-  EXPECT_EQ(expected, nextPrime(value));
+  EXPECT_EQ(29, nextPrime(4));
 }
 
 
