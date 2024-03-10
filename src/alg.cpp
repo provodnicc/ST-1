f9e868359711e83bc8bdfae8a8583294c26f5a64
Pavel Smirnov <pasha1201s@inbox.ru>
2024-03-10 12:06:36 +0300

    feat(lab1): init tests and number Lib

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,19 +2,55 @@
 #include <cstdint>
 #include "alg.h"
 
-
-bool checkPrime(uint64_t value) {
-// вставьте код функции
+bool checkPrime(uint64_t value)
+{
+    if (value <= 1)
+        return false;
+    for (uint64_t i = 2; i * i <= value; i++)
+    {
+        if (value % i == 0)
+            return false;
+    }
+    return true;
 }
 
-uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+uint64_t nPrime(uint64_t n)
+{
+    uint64_t num = 0;
+    uint64_t res = 0;
+    while (n > 0)
+    {
+        if (checkPrime(num))
+        {
+            res = num;
+            --n;
+        }
+        ++num;
+    }
+    return res;
 }
 
-uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+uint64_t nextPrime(uint64_t value)
+{
+    while (true)
+    {
+        ++value;
+        if (checkPrime(value))
+        {
+            return value;
+        }
+    }
 }
 
-uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+uint64_t sumPrime(uint64_t hbound)
+{
+    uint64_t sum = 0;
+    for (int i = 0; i < hbound; i++)
+    {
+        if (checkPrime(i))
+        {
+            sum += i;
+        }
+    }
+    return sum;
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,37 @@
-
+// Copyright 2024 Pavel Smirnov
 #include <gtest/gtest.h>
 #include <cstdint>
 #include "alg.h"
 
 
+TEST(PrimeTest, CheckPrimeWithPrimeNumber) {
+  uint64_t number = 13;
+  EXPECT_TRUE(checkPrime(number));
+}
+
+
+TEST(PrimeTest, CheckPrimeWithCompositeNumber) {
+  uint64_t number = 20;
+  EXPECT_FALSE(checkPrime(number));
+}
+
+
+TEST(PrimeTest, NthPrimeNumber) {
+  uint64_t number = 6;
+  uint64_t expected = 13;
+  EXPECT_EQ(expected, nPrime(n));
+}
+
+
+TEST(PrimeTest, NextPrimeNumber) {
+  uint64_t value = 24;
+  uint64_t expected = 29;
+  EXPECT_EQ(expected, nextPrime(value));
+}
+
+
+TEST(PrimeTest, SumOfPrimeNumbers) {
+  uint64_t maxBound = 20;
+  uint64_t expectedSum = 2 + 3 + 5 + 7 + 11 + 13 + 17 + 19;
+  EXPECT_EQ(expectedSum, sumPrime(maxBound));
+}
